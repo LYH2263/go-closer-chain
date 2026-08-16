3afc2bfe7401c880da2a0fcdb35f67cc5c3abd35
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-17 00:04:43 +0800

    fix: 串联 Close 中断

--- internal/closekit/closekit.go
+++ internal/closekit/closekit.go
@@ -3,10 +3,11 @@ package closekit
 import "io"
 
 func CloseAll(closers ...io.Closer) error {
+	var first error
 	for _, c := range closers {
-		if err := c.Close(); err != nil {
-			return err // BUG: stop early
+		if err := c.Close(); err != nil && first == nil {
+			first = err
 		}
 	}
-	return nil
+	return first
 }
